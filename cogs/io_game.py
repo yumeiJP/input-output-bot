3c8fa37e8cf247d2f69231d23d0502418fd55516
yumeiJP <tk5138@email.kist.ed.jp>
2026-04-01 14:11:02 +0900

    rename things and remove redundancies

--- cogs/io_game.py
+++ cogs/io_game.py
@@ -13,11 +13,10 @@ class Game:  # pylint: disable=too-many-instance-attributes
         self.channel = channel
         self.initiator_user_id = initiator_user_id
         self.max_queries_per_round = max_queries_per_round
-        self.players = {}
-        self.join_order = []
+        self.players_score = {}
         self.recruitment_task = None
         self.recruitment_start = None
-        self.participant_list = []
+        self.player_list = []
         self.rounds = []
         self.current_round_index = -1
 
@@ -39,7 +38,7 @@ class PlayerRoundState:
 
     def __init__(self, user_id):
         self.user_id = user_id
-        self.potential_points = 1000.0
+        self.query_count = 0
         self.solved = False
 
 
@@ -51,8 +50,35 @@ class IOGame(commands.Cog):
         self.bot = bot
         self.games = {}
 
+    async def start_game(self, game):
+        # will implement
+        pass
+
     async def end_recruitment(self, game):
         """Called after recruitment ends"""
+
+        if game.recruitment_task:
+            game.recruitment_task.cancel()
+
+        if len(game.players_score) < 1:
+
+            try:
+                await game.channel.send("Not enough players joined!")
+            except Exception as e:
+                print(f"Error sending message: {e}")
+
+            del self.games[game.channel.id]
+            return
+
+        # Indicate round starting
+        game.current_round_index = 0
+
+        try:
+            await game.channel.send("Game has started!")
+        except Exception as e:
+            print(f"Error sending message: {e}")
+
+        await self.start_game(game)
 
     @commands.command()
     async def create(self, ctx, max_queries: int = 20):
@@ -61,7 +87,9 @@ class IOGame(commands.Cog):
             await ctx.send("There is already a game starting!")
             return
 
-        if not (max_queries >= 1 and isinstance(max_queries, int)):
+        if (
+            not (max_queries >= 1 and isinstance(max_queries, int))
+        ) or max_queries is None:
             await ctx.send("Please send how many max queries you want!")
             return
 
@@ -75,7 +103,38 @@ class IOGame(commands.Cog):
         game.recruitment_task = asyncio.create_task(recruitment_timeout())
         game.recruitment_start = datetime.utcnow()
 
+        player_id = ctx.author.id
+
+        game.player_list.append(player_id)
+        game.players_score[player_id] = 0.0
+
         await ctx.send("Game created! Use -join to join!")
+
+    @commands.command()
+    async def join(self, ctx):
+        """Allows players to join the game started"""
+
+        game = self.games.get(ctx.channel.id)
+
+        if game is None:
+            await ctx.send("There is no game to join! Try -create to start a game!")
+            return
+
+        if game.current_round_index != -1:
+            await ctx.send("The game already started! Please wait till it ends.")
+            return
+            # TODO: allow players to join regardless, starting with 0 points though.
+
+        player_id = ctx.author.id
+
+        if player_id in game.players_score:
+            await ctx.send("You are already in the game!")
+            return
+
+        game.player_list.append(player_id)
+        game.players_score[player_id] = 0.0
+
+        await ctx.send("You are now in the game!")
 
 
 async def setup(bot):
